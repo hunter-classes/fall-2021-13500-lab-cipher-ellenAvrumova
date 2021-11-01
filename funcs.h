70b832c116a0ce0dd610740ca8c3b483200df188
Ellen Avrumova <ellenavrumova@Ellens-Air.home>
2021-10-31 23:23:46 -0400

    Completed solve

--- funcs.h
+++ funcs.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <string>
 
+char shiftChar(char c, int rshift);
+std::string encryptCaesar(std::string plaintext, int rshift);
+double distance(std::vector<double> first, std::vector<double> second);
 std::string solve(std::string encrypted_string);
--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <vector>
 #include "funcs.h"
 
 int main() {
-  solve("hellohello");
+  std::cout << solve("ebiil") << std::endl;
+   std::cout << solve("ebiil ebiil&") << std::endl; //hello hello&
+   std::cout << solve("vsxu Dy dro Zkcd") << std::endl; //link To the Past
+   std::cout << solve("abzOvmqzlli") << std::endl; //decRyptcool
+  std::cout << solve("edbobemuh") << std::endl; //onlylower
+  std::cout << solve("Hvwg zop qwdvsf wg dfshhm vofr tcf gcas fsogcb.") << std::endl; //rotate 14
+  std::cout<<solve("Iye kbo fobi lokedspev")<<"\n"; //rotate 10
+  std::cout << solve("purpxatVs pbqr vfBssol SbhEE")<<"\n"; //rotate 13
+  std::cout<<solve("l oryh hdwlqj slccd")<<"\n";
+  
   return 0;
 }
