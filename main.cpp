4f2e214bd7e9152a8bb295e3c735ab20f5986449
Ellen Avrumova <ellenavrumova@Ellens-Air.home>
2021-11-03 00:26:37 -0400

    Formatted main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+/**
+ * Author: Ellen Avrumova
+ * Course: CSCI-135
+ * Instructor: Tong Yi & Mike Zamansky
+ * Assignment: Lab Cipher
+ * This is the main file for Lab Cipher.
+*/
+
 #include <iostream>
 #include "funcs.h"
 
 int main() {
-  
+  std::cout << "ENCRYPTED TEXT:\n" << "Xli tsmrx sj ywmrk Psviq Mtwyq mw xlex mx lew e qsvi-sv-piww rsvqep hmwxvmfyxmsr sj pixxivw, ew sttswih xs ywmrk 'Gsrxirx livi, gsrxirx livi', qeomrk mx psso pmoi viehefpi Irkpmwl." << std::endl;
+  std::cout << "SOLVED TEXT:\n" << solve("Xli tsmrx sj ywmrk Psviq Mtwyq mw xlex mx lew e qsvi-sv-piww rsvqep hmwxvmfyxmsr sj pixxivw, ew sttswih xs ywmrk 'Gsrxirx livi, gsrxirx livi', qeomrk mx psso pmoi viehefpi Irkpmwl.") << std::endl;
+  std::cout << std::endl;
+  std::cout << "ENCRYPTED TEXT:\n" << "Htvtapotl td l qcpp nzyepye, xfwetwtyrflw zywtyp pynjnwzapotl hcteepy lyo xltyeltypo mj l nzxxfytej zq gzwfyeppcd esczfrs l xzopw zq zapy nzwwlmzcletzy, fdtyr l htvt-mldpo potetyr djdepx." << std::endl;
+  std::cout << "SOLVED TEXT:\n" << solve("Htvtapotl td l qcpp nzyepye, xfwetwtyrflw zywtyp pynjnwzapotl hcteepy lyo xltyeltypo mj l nzxxfytej zq gzwfyeppcd esczfrs l xzopw zq zapy nzwwlmzcletzy, fdtyr l htvt-mldpo potetyr djdepx.") << std::endl;
+  std::cout << std::endl;
+  std::cout << "ENCRYPTED TEXT:\n" << "Qcadihsf gqwsbqs wg hvs ghirm ct ozucfwhvawq dfcqsggsg, qcadihohwcboz aoqvwbsg obr qcadihohwcb whgszt." << std::endl;
+  std::cout << "SOLVED TEXT:\n" << solve("Qcadihsf gqwsbqs wg hvs ghirm ct ozucfwhvawq dfcqsggsg, qcadihohwcboz aoqvwbsg obr qcadihohwcb whgszt.") << std::endl;
   return 0;
 }
